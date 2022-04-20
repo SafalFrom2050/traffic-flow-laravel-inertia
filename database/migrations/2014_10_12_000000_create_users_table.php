e4b26c3fb937374b5fa6d7f317c27714373c2b7d
safal <safal.sharma.ss@gmail.com>
2022-04-21 03:58:25 +0545

    add dashboard and other fixes.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('phone')->unique()->nullable();
             $table->date('dob')->nullable();
-            $table->boolean('is_approved')->default(0);
+            $table->boolean('is_approved')->default(false);
             $table->integer('reward_points')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
