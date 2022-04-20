d8b218aebb1c86fb6307d4bc08b655fee8238cde
safal <safal.sharma.ss@gmail.com>
2022-04-21 00:45:09 +0545

    manage vehicle, road trips relations.

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Vehicle extends Model
 {
     protected $guarded = ['id'];
+
+    protected $with = ['roadTrip'];
+
+    public function roadTrip(): BelongsTo
+    {
+        return $this->belongsTo(RoadTrip::class);
+    }
 }
